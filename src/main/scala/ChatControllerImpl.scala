8775d9c072b6c2afef9faa51b91b85953097e957
svvitov <uildzhek666@gmail.com>
2021-10-12 20:17:41 +0400

    final

--- src/main/scala/ChatControllerImpl.scala
+++ src/main/scala/ChatControllerImpl.scala
@@ -1,4 +1,4 @@
-import User.UserBehavior
+import User.{MySerializable, PrivateMessage, PublicMessage}
 import akka.actor.typed.ActorSystem
 import akka.cluster.typed.Cluster
 import com.typesafe.config.ConfigFactory
@@ -8,17 +8,26 @@ import javafx.event.ActionEvent
 class ChatControllerImpl extends ChatController{
 
   var login: String = _ // var, потому что сюда передается login из окна входа
-  var system: ActorSystem[String] = _
-  val controller = this
+  var system: ActorSystem[MySerializable] = _
 
   override def onSendMessageButton(event: ActionEvent): Unit = {
     val nickname = login // создаю val на основе var login, чтобы не отправлять var
     val message = messageInput.getText.trim
+    if (message != "") {
+      message match {
+        //приватное сообщение /private;получатель; текст
+        case input: String if message.contains("/private") =>
+          val command = input.split(";").toVector
+          val to = command(1)
+          val text = command(2)
+          system ! PrivateMessage(nickname, to, text)
 
-    system ! message
+        case _ => system ! PublicMessage(nickname, message)
+      }
+      messageInput.clear()
+    }
 
 
-    messageInput.clear()
   }
 
   def start(port: String): Unit = {
@@ -30,14 +39,15 @@ class ChatControllerImpl extends ChatController{
             """).withFallback(ConfigFactory.load())
       this.system = ActorSystem(Main(this), "ClusterSystem", config)
       val cluster = Cluster(this.system)
+      messagesField.appendText(s"Вы онлайн как: $login\nЧтобы отправить приватное сообщение используйте команду /private;NicknameПолучателя;Текст сообщения\n\n")
     }
   }
 
 
   override def onExitButton(event: ActionEvent): Unit = {
+    this.system.terminate()
     Platform.exit()
     System.exit(0)
   }
-
 
 }
--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -1,18 +1,18 @@
-import User.{MySerializable, PublicMessage}
+import User.MySerializable
 import akka.actor.typed.Behavior
 import akka.actor.typed.pubsub.Topic
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 
 object Main{
-  def apply(chatControllerImpl: ChatControllerImpl): Behavior[String] = start(chatControllerImpl)
+  def apply(chatControllerImpl: ChatControllerImpl): Behavior[MySerializable] = start(chatControllerImpl)
 
-  def start(chatControllerImpl: ChatControllerImpl):Behavior[String] = Behaviors.setup({context =>
+ private def start(chatControllerImpl: ChatControllerImpl):Behavior[MySerializable] = Behaviors.setup({context =>
     val actor = context.spawn(User(chatControllerImpl), "user")
     val topic = context.spawn(Topic[MySerializable]("chat"), "chat")
     topic ! Topic.Subscribe(actor)
 
     Behaviors.receiveMessage{
-      case string: String => topic ! Topic.Publish(PublicMessage(from = "test", string))
+      case msg: MySerializable => topic ! Topic.Publish(msg)
         Behaviors.same
     }
   })
@@ -21,9 +21,7 @@ object Main{
 
   object User {
     trait MySerializable
-
     final case class PublicMessage(from: String, text: String) extends MySerializable
-
     final case class PrivateMessage(from: String, to: String, text: String) extends MySerializable
 
     def apply(chatControllerImpl: ChatControllerImpl): Behavior[MySerializable] = createActor(chatControllerImpl)
@@ -38,7 +36,8 @@ object Main{
         msg match {
           case PublicMessage(from, text) => chatControllerImpl.messagesField.appendText(s"$from: $text\n")
             this
-          case PrivateMessage(from, to, text) => println(s"$from: $text")
+          case PrivateMessage(from, to, text) => if (from.equals(chatControllerImpl.login) | to.equals(chatControllerImpl.login))
+            chatControllerImpl.messagesField.appendText(s"[Private] $from: $text")
             this
         }
       }
